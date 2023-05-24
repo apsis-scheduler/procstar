c922499c9b5af4cef477403caf1c7d728d559486
Alex Samuel <alex@alexsamuel.net>
2023-05-24 11:19:49 -0700

    Our own rusage struct.

--- src/res.rs
+++ src/res.rs
@@ -9,74 +9,55 @@ use serde::Serialize;
 
 //------------------------------------------------------------------------------
 
-/// Mirror types to derive Serialize for libc remote types.
-/// https://serde.rs/remote-derive.html
-mod libc_serde {
+/// Analogue to libc's `struct rusage`.  We use our own struct,
+///
+/// - to omit fields not used by Linux
+/// - to convert some values to more convenient units
+/// - because `libc::rusage` doesn't implement `serde::Serialize`.
+///
+#[derive(Debug, Serialize)]
+pub struct ResourceUsage {
+    /// User CPU time used.
+    pub utime: f64,
+    /// System CPU time used.
+    pub stime: f64,
+    /// Maximum resident set size.
+    pub maxrss: u64,
+    /// Page reclaims (minor / soft page faults).
+    pub minflt: u64,
+    /// Page faults (major / hard page faults).
+    pub majflt: u64,
+    /// Swaps.
+    pub nswap: u64,
+    /// Block input operations.
+    pub inblock: u64,
+    /// Block output operations.
+    pub oublock: u64,
+    /// Voluntary context switches.
+    pub nvcsw: u64,
+    /// Involuntary context switches.
+    pub nivcsw: u64,
+}
 
-    use libc::{c_long, suseconds_t, time_t};
-    use serde::{Deserialize, Serialize};
+fn time_to_sec(time: libc::timeval) -> f64 {
+    time.tv_sec as f64 + 1e-6 * time.tv_usec as f64
+}
 
-    #[derive(Serialize, Deserialize)]
-    #[serde(remote = "libc::timeval")]
-    pub struct Timeval {
-        pub tv_sec: time_t,
-        pub tv_usec: suseconds_t,
+impl ResourceUsage {
+    pub fn new(r: &rusage) -> Self {
+        Self {
+            utime: time_to_sec(r.ru_utime),
+            stime: time_to_sec(r.ru_stime),
+            maxrss: (r.ru_maxrss as u64) * 1024,  // convert KiB to bytes
+            minflt: r.ru_minflt as u64,
+            majflt: r.ru_majflt as u64,
+            nswap: r.ru_nswap as u64,
+            inblock: r.ru_inblock as u64,
+            oublock: r.ru_oublock as u64,
+            nvcsw: r.ru_nvcsw as u64,
+            nivcsw: r.ru_nivcsw as u64,
+        }
     }
-
-    #[derive(Serialize, Deserialize)]
-    #[serde(remote = "libc::rusage")]
-    pub struct Rusage {
-        #[serde(with = "Timeval")]
-        pub ru_utime: libc::timeval,
-        #[serde(with = "Timeval")]
-        pub ru_stime: libc::timeval,
-        pub ru_maxrss: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad1: u32,
-        pub ru_ixrss: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad2: u32,
-        pub ru_idrss: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad3: u32,
-        pub ru_isrss: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad4: u32,
-        pub ru_minflt: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad5: u32,
-        pub ru_majflt: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad6: u32,
-        pub ru_nswap: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad7: u32,
-        pub ru_inblock: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad8: u32,
-        pub ru_oublock: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad9: u32,
-        pub ru_msgsnd: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad10: u32,
-        pub ru_msgrcv: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad11: u32,
-        pub ru_nsignals: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad12: u32,
-        pub ru_nvcsw: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad13: u32,
-        pub ru_nivcsw: c_long,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-        __pad14: u32,
-
-        #[cfg(any(target_env = "musl", target_os = "emscripten"))]
-        __reserved: [c_long; 16],
-    }
-
 }
 
 //------------------------------------------------------------------------------
@@ -163,40 +144,26 @@ pub struct ProcRes {
     /// The pid with which the process ran.
     pub pid: pid_t,
 
+    /// Process status, if it has completed.
     pub status: Option<Status>,
+
+    /// Process resource usage, if it has completed.
+    pub rusage: Option<ResourceUsage>,
 
     /// Fd results.
     /// FIXME: Associative map from fd instead?
     pub fds: BTreeMap<String, FdRes>,
-
-    /// Resource usage for the process itself.
-    #[serde(with = "libc_serde::Rusage")]
-    pub rusage: rusage,
-}
-
-fn time_to_sec(time: libc::timeval) -> f64 {
-    time.tv_sec as f64 + 1e-6 * time.tv_usec as f64
 }
 
 impl ProcRes {
-    pub fn new(errors: Vec<String>, pid: pid_t, status: c_int, rusage: rusage) -> ProcRes {
-        ProcRes {
+    pub fn new(errors: Vec<String>, pid: pid_t, status: c_int, rusage: rusage) -> Self {
+        Self {
             errors,
             pid,
             status: Some(Status::new(status)),
+            rusage: Some(ResourceUsage::new(&rusage)),
             fds: BTreeMap::new(),
-            rusage,
         }
-    }
-
-    /// User time in s.
-    pub fn utime(&self) -> f64 {
-        time_to_sec(self.rusage.ru_utime)
-    }
-
-    /// System time in s.
-    pub fn stime(&self) -> f64 {
-        time_to_sec(self.rusage.ru_stime)
     }
 }
 

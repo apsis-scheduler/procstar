955965438de78423dd49b6bdfb88c9ea0a01a73f
Alex Samuel <alex@alexsamuel.net>
2023-05-26 09:28:33 -0700

    Factor out response fn.

--- src/http.rs
+++ src/http.rs
@@ -7,24 +7,30 @@ use crate::procs::SharedRunningProcs;
 
 //------------------------------------------------------------------------------
 
+type Req = Request<Incoming>;
+// FIXME: Wrap JSON response in a body type.  Set content-type.
+type Rsp = Response<Full<Bytes>>;
+
+async fn procs_get(procs: SharedRunningProcs, _req: Req) -> Result<Rsp, hyper::Error> {
+    let res = procs.to_result();
+    let body = serde_json::to_string(&res).unwrap();
+    Ok::<_, hyper::Error>(hyper::Response::new(Full::<Bytes>::from(body)))
+}
+
+//------------------------------------------------------------------------------
+
 /// Runs the HTTP service.
-pub async fn run_http(running_procs: SharedRunningProcs) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn run_http(procs: SharedRunningProcs) -> Result<(), Box<dyn std::error::Error>> {
     let addr: std::net::SocketAddr = ([127, 0, 0, 1], 3000).into();
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
     eprintln!("Listening on http://{}", addr);
     loop {
         let (stream, _) = listener.accept().await?;
+        let procs = procs.clone();
 
-        let running_procs = running_procs.clone();
-        let service = hyper::service::service_fn(move |_req: Request<Incoming>| {
-            let running_procs = running_procs.clone();
-            async move {
-                // let body = format!("{}\n", running_procs.len());
-                let res = running_procs.to_result();
-                let body = serde_json::to_string(&res).unwrap();
-                Ok::<_, hyper::Error>(hyper::Response::new(Full::<Bytes>::from(body)))
-            }
+        let service = hyper::service::service_fn(move |req: Req| {
+            procs_get(procs.clone(), req)
         });
 
         tokio::task::spawn_local(async move {

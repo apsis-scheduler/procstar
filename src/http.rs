4221215488ad266ecfafba8b5d778730fa35bff9
Alex Samuel <alex@alexsamuel.net>
2023-05-26 10:57:23 -0700

    Wrap JSON response.

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,6 @@
 use http_body_util::Full;
 use hyper::body::{Bytes, Incoming};
+use hyper::header::HeaderValue;
 use hyper::{Request, Response, StatusCode, Method};
 
 use crate::procs::SharedRunningProcs;
@@ -7,13 +8,20 @@ use crate::procs::SharedRunningProcs;
 //------------------------------------------------------------------------------
 
 type Req = Request<Incoming>;
-// FIXME: Wrap JSON response in a body type.  Set content-type.
 type Rsp = Response<Full<Bytes>>;
 
+fn make_json_response<T>(status: StatusCode, obj: T) -> Rsp
+    where T: serde::Serialize
+{
+    let body = serde_json::to_string(&obj).unwrap();
+    let mut rsp = hyper::Response::new(Full::<Bytes>::from(body));
+    *rsp.status_mut() = status;
+    rsp.headers_mut().insert(hyper::header::CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    rsp
+}
+
 async fn procs_get(procs: SharedRunningProcs, _req: Req) -> Result<Rsp, hyper::Error> {
-    let res = procs.to_result();
-    let body = serde_json::to_string(&res).unwrap();
-    Ok::<_, hyper::Error>(hyper::Response::new(Full::<Bytes>::from(body)))
+    Ok(make_json_response(StatusCode::OK, procs.to_result()))
 }
 
 fn error(req: Req) -> Result<Rsp, hyper::Error> {

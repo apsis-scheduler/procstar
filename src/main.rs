62b4bb32adfc654e955c5f4c985eae5bfd3b1f9c
Alex Samuel <alex@alexsamuel.net>
2023-09-19 16:35:00 -0400

    Hacky way to run http and ws at the same time.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,27 @@ extern crate exitcode;
 
 mod argv;
 
+use futures::stream::FuturesUnordered;
+use futures::stream::StreamExt;
+
 // use procstar::fd::parse_fd;
-use procstar::http::run_http;
+use procstar::http;
 use procstar::procs::{collect_results, start_procs, SharedRunningProcs};
 use procstar::res;
 use procstar::spec;
 use procstar::wsclient;
 
 //------------------------------------------------------------------------------
+
+async fn run_http(running_procs: SharedRunningProcs) {
+    http::run_http(running_procs).await.unwrap(); // FIXME: unwrap
+}
+
+async fn run_ws(url: String, running_procs: SharedRunningProcs) {
+    let url = url::Url::parse(&url).unwrap(); // FIXME: unwrap
+    let (_connection, handler) = wsclient::Connection::connect(&url).await.unwrap(); // FIXME: unwrap
+    handler.run(running_procs.clone()).await.unwrap();  // FIXME: unwrap
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -26,29 +39,29 @@ async fn main() {
     };
 
     let local = tokio::task::LocalSet::new();
-    // FIXME: Are HTTP serve and websockets actually exclusive?
-    if args.serve {
-        // Service mode.
-        local
-            .run_until(async move {
-                // Start specs from the command line.  Discard the tasks.  We
-                // intentionally don't start the HTTP service until the input
-                // processes have started, to avoid races where these procs
-                // don't appear in HTTP results.
-                start_procs(input, running_procs.clone()).await;
-                // Run the HTTP service.
-                run_http(running_procs).await.unwrap()
-            })
-            .await;
-    } else if let Some(url) = args.connect {
-        let url = url::Url::parse(&url).unwrap(); // FIXME: unwrap
-        local
-            .run_until(async move {
-                start_procs(input, running_procs.clone()).await;
-                let (_connection, handler) = wsclient::Connection::connect(&url).await.unwrap(); // FIXME: unwrap
-                handler.run(running_procs.clone()).await.unwrap(); // FIXME: unwrap
-            })
-            .await;
+
+    if args.serve || args.connect.is_some() {
+        // Start specs from the command line.  Discard the tasks.  We
+        // intentionally don't start the HTTP service until the input
+        // processes have started, to avoid races where these procs
+        // don't appear in HTTP results.
+        local.run_until(start_procs(input, running_procs.clone())).await;
+
+        let mut futs = FuturesUnordered::<_>::new();
+
+        if args.serve {
+            // Run the HTTP service.
+            futs.push(futures::future::Either::Right(run_http(running_procs.clone())));
+        }
+
+        if let Some(url) = args.connect {
+            futs.push(futures::future::Either::Left(run_ws(url, running_procs.clone())));
+        }
+
+        local.run_until(async move {
+            while futs.next().await.is_some() {
+            }
+        }).await;
     } else {
         local
             .run_until(async move {

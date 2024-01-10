143040120620550cfddc1d156e33c72f3ef7802d
Alex Samuel <alex@alexsamuel.net>
2024-01-10 18:20:29 -0500

    CentOS7 and conda build mechanism.

---
+++ tools/centos7/build-conda.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+# Builds Procstar in a CentOS7 container and wraps it into a conda package.
+#
+# Usage: build-conda.sh VERSION
+#
+# where v$VERSION is a git tag.
+
+version=$1
+if [[ -z "$version" ]]; then
+    echo "Usage: $0 VERSION" >&2
+    exit 1
+fi
+
+dir=$(dirname $(realpath $0))
+root="$(dirname $(dirname $dir))"
+
+target=$root/target/centos7-conda
+
+# Clean up previous build products.
+rm -rf $target
+mkdir -p $target
+
+# Make sure the build container image is ready.
+podman build $dir -t centos7-cargo
+
+# Build.
+podman run --rm \
+       -v "$dir/conda-recipe:/source" -v "$target:/conda/conda-bld" \
+       --workdir /root \
+       centos7-cargo \
+       conda build /source
+
+echo
+echo conda package: $(ls $target/linux-64/*.tar.bz2)
+
---
+++ tools/centos7/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Builds Procstar in a CentOS7 container.
+
+dir=$(dirname $(realpath $0))
+root="$(dirname $(dirname $dir))"
+target=$root/target/centos7
+
+# Clean up previous build products.
+rm -rf $target
+mkdir -p $target
+
+# Make sure the build container image is ready.
+podman build $dir -t centos7-cargo
+
+# Build.
+podman run -t --rm -v "$root:/source" -v "$target:/source/target" centos7-cargo \
+       cargo build --release
+

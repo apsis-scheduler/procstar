c8f6f1f4e5888b1b917fd13b78c74c96c22f34ab
Alex Samuel <alex@alexsamuel.net>
2024-04-26 10:55:00 -0700

    Clean up docstrings.

--- src/res.rs
+++ src/res.rs
@@ -83,15 +83,17 @@ pub enum FdRes {
         encoding: String,
     },
 
-    /// UTF-8-encoded output.
+    /// Encoded text output.
     Text {
         text: String,
+        /// Text encoding.  Currently always "utf-8".
         encoding: String,
     },
 
-    /// Base64-encoded output.
     Data {
+        /// Encoded binary data.
         data: String,
+        /// Binary data encoding.  Currently always "base64".
         encoding: String,
     },
 }

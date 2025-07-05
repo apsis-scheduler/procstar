8274d12200429477196dd8fe64ba0d48828567d5
Augustus Ostow <ostowster@gmail.com>
2025-07-05 18:33:49 -0400

    Fix test_spec

--- test/unit/test_spec.py
+++ test/unit/test_spec.py
@@ -2,7 +2,9 @@ import procstar.spec
 
 
 def test_to_jso():
-    jso = procstar.spec.Proc(["/usr/bin/sleep"]).to_jso()
+    jso = procstar.spec.Proc(
+        ["/usr/bin/sleep"], systemd_properties=procstar.spec.Proc.SystemdProperties()
+    ).to_jso()
     assert jso["argv"] == ("/usr/bin/sleep",)
-    assert set(jso.keys())  == {"argv", "env", "fds", "systemd_properties"}
+    assert set(jso.keys()) == {"argv", "env", "fds", "systemd_properties"}
     assert set(jso["systemd_properties"].keys()) == {"scope", "slice"}
